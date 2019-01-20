2c56ad6567981a5eedf6f9c35660d3c8b91fe86c
linkseed <linkseed@gmail.com>
2019-01-20 15:30:37 +0900

    fix helptags command

--- pearl-config/install.sh
+++ pearl-config/install.sh
@@ -1,15 +1,17 @@
 function post_install(){
     local giturl=@git_url
+    local pkgdir="${PEARL_PKGVARDIR}/pack/${PEARL_PKGREPONAME}/start"
 
     # Install/update the dependency here:
     # pearl emerge PEARL_REPO_NAME/mydep
 
     info "Installing or updating the ${PEARL_PKGNAME} git repository..."
-    mkdir -p "${PEARL_PKGVARDIR}/pack/${PEARL_PKGREPONAME}/start"
-    install_or_update_git_repo $giturl "${PEARL_PKGVARDIR}/pack/${PEARL_PKGREPONAME}/start/${PEARL_PKGNAME}" master
-    # if [ `which vim` ]; then
-    #     vim -u NONE -c "helptags ${PEARL_PKGVARDIR}/pack/${PEARL_PKGREPONAME}/start/${PEARL_PKGNAME}/doc" -c q
-    # fi
+    mkdir -p $pkgdir
+    install_or_update_git_repo $giturl "$pkgdir/${PEARL_PKGNAME}" master
+    if [ -d "$pkgdir/${PEARL_PKGNAME}/doc" ] && [ `which vim` ]; then
+        info "Generating the help tags..."
+        vim -u NONE -c "helptags $pkgdir/${PEARL_PKGNAME}/doc" -c q
+    fi
 
     return 0
 }
